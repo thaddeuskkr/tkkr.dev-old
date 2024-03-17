d87f2270dc883587cfcd1aaa34f9be6cecb88c04
Thaddeus Kuah <tk@tkkr.dev>
2024-03-17 06:49:51 +0000

    chore: try something
    Signed-off-by: Thaddeus Kuah <tk@tkkr.dev>

--- index.js
+++ index.js
@@ -40,14 +40,3 @@ function closecontact() {
         $('#contact_container').css('display', 'none');
     }, 1000);
 }
-setTimeout(function () {
-    $('#loading').addClass('animated fadeOut');
-    setTimeout(function () {
-        $('#loading').removeClass('animated fadeOut');
-        $('#loading').css('display', 'none');
-        $('#box').css('display', 'none');
-        $('#about').removeClass('animated fadeIn');
-        $('#contact').removeClass('animated fadeIn');
-        $('#projects').removeClass('animated fadeIn');
-    }, 1000);
-}, 1500);
